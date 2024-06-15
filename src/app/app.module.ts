c0e1074199f862d50935c231aee82a656fd670ef
Ramiro Grisales <ramiro.grisales@centrohub.co>
2024-06-15 15:05:18 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/components/counter.module';
 import { HeroModule } from './heroes/hero.module';
 
